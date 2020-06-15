c869850d6d80c83884677a7d5a663efc03e0ee4a
Ferdian Noprianto <ferdiannoprianto@gmail.com>
2020-06-16 03:44:00 +0900

    Menambahkan main view dan konfigurasi route

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+@extends('template/main')
+
+@section('tittle', 'About')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-2">Roselia</h1>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,13 @@
+@extends('template/main')
+
+@section('tittle', 'Ferdian Noprianto')
+
+@section('container')
+<div class="container">
+    <div class="row">
+        <div class="col-10">
+            <h1 class="mt-2">Hello Happy world</h1>
+        </div>
+    </div>
+</div>
+@endsection
